9804390f563b66d907a249140ba88730e465f725
Márcio Bueno <marciobueno@gmail.com>
2021-10-07 19:11:11 -0300

    Corrige problema de renderização desnecesária por causa do context

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import React, { useState, useEffect, useCallback } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
 
-import { Toolbar } from './components/Toolbar';
-import { ThemeContext, themes } from './ThemeContext';
-import { ThemedButton } from './components/ThemedButton';
+import { Toolbar } from "./components/Toolbar";
+import { ThemeContext, themes } from "./ThemeContext";
+import { ThemedButton } from "./components/ThemedButton";
 
 export default function App() {
+  const [viewVisible, setViewVisible] = useState(false);
   const [theme, setTheme] = useState(themes.dark);
-  function toggleTheme() {
+
+  const toggleTheme = useCallback(() => {
     setTheme(theme === themes.dark ? themes.light : themes.dark);
-  }
+  }, [theme]);
+
+  const [contextValue, setContextValue] = useState({
+    theme: theme,
+    toggleTheme: toggleTheme,
+  });
+
+  useEffect(() => {
+    setContextValue({
+      theme: theme,
+      toggleTheme: toggleTheme,
+    });
+  }, [toggleTheme]);
+
+  const handleVisible = () => setViewVisible(!viewVisible);
 
   return (
     <View style={styles.container}>
-      <ThemeContext.Provider value={{ theme: theme, toggleTheme: toggleTheme }}>
+      <ThemeContext.Provider value={contextValue}>
         <Toolbar />
       </ThemeContext.Provider>
-      <ThemedButton title="Fora" onPress={toggleTheme} />
+      {viewVisible && <View style={styles.view} />}
+      <ThemedButton title="Fora" onPress={handleVisible} />
       <StatusBar style="auto" />
     </View>
   );
@@ -26,8 +43,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  view: {
+    width: 150,
+    height: 70,
+    backgroundColor: "green",
   },
 });
